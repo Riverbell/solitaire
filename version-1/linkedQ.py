9e7df8856dde29a818a93249ea3a40ee7c6651d7
Riverbell <emba93@live.se>
2016-03-19 00:35:44 +0100

    Translated comments

--- version-1/linkedQ.py
+++ version-1/linkedQ.py
@@ -1,19 +1,17 @@
 class Node():
-	"""Klassen består av noder som håller ihop listan genom att peka på nästa nod."""
+	"""Nodes have a value and a pointer. The pointer keeps the list (queue) together"""
 	def __init__(self, value, next = None):
-		"""Skapar klassens attribut."""
 		self.value = value
 		self.next = next
 
 class LinkedQ():
-	"""Arrangerar en kö."""
+	"""Arranges a queue"""
 	def __init__(self, first = None, last = None):
-		"""Vi skapar klassens attribut."""
 		self.first = first
 		self.last = last
 
 	def __str__(self):
-		"""Metoden returnerar en sträng som innehåller användarens inmatade kort."""
+		"""Returns a string with the users input"""
 		s = ""
 		p = self.first
 		while p != None:
@@ -22,22 +20,22 @@ class LinkedQ():
 		return s
 
 	def put(self,x):
-		"""Metoden tar emot variabeln x och placerar den sist i kön."""
-		if self.isEmpty(): # Om lista är tom skapar vi en nod som är listans första och sista nod. 
+		"""Takes the variable x as parameter and places it last in the queue"""
+		if self.isEmpty(): # If the list is empty, create a node which is the first and last node in the list
 			newNode = Node(x)
 			self.first = newNode
 			self.last = newNode
-		else: # Om listan inte är tom skapar vi en ny nod. Noden är den sista i listan och pekar på None.
+		else: # If the list isn't empty, create new node. Last in the list and points to None
 			newNode = Node(x)
 			self.last.next = newNode
 			self.last = newNode
 
 	def get(self):
-		"""Metoden plockar ut och returnerar det som står först i listan, dvs i kön."""
-		if not self.isEmpty(): # Om listan inte är tom sparar vi det första elementet i listan som variabeln 'first' och ersätter det genom att ge 
-			first = self.first # elementet nummer 2 första platsen.
+		"""Takes the first item of the list out of the list and returns it"""
+		if not self.isEmpty(): 
+			first = self.first 
 			self.first = self.first.next
-			return first.value # Vi returnerar det första elementet.
+			return first.value 
 		elif self.isEmpty():
 			return "!"
 
@@ -49,8 +47,8 @@ class LinkedQ():
 			return "!"
 
 	def isEmpty(self):
-		"""Metoden tar emot en lista och returnerar True om kön (listan) är tom, i övriga fall returnerar metoden False."""
-		if self.first == None: # Om listan saknar ett första element är listan tom och metoden returnerar 'True'.
+		"""Takes a list and checks if it's empty or not"""
+		if self.first == None: 
 			return True
 		else:
-			return False # I övriga fall returnerar metoden 'False'.
+			return False 
